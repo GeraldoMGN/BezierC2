3c02ac5e29243fae6f64b1e8708dd55e3a82e7a3
GeraldoMGN <gmgn@cin.ufpe.br>
2018-12-13 21:55:07 -0200

    Main do app

--- bezier.js
+++ bezier.js
@@ -1,6 +1,19 @@
 function bezier(){
     var canvas = document.getElementById("app");
     var context = canvas.getContext("2d");
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(main);
+
+    function main(){
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+
+        context.fillStyle = "black";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = 'rgb(255, 255, 255)';
+		context.font = "12px Courier";
+        context.fillText(canvas.width, 8, canvas.height - 10);
+        context.fillText(canvas.height, 8, canvas.height - 24);
+
+        window.requestAnimationFrame(main);
+    }
 }
