7bb4ec4272e0d20ad657d812b7e5f24f00ebb339
GeraldoMGN <gmgn@cin.ufpe.br>
2018-12-13 21:33:12 -0200

    Estrutura básica da página

---
+++ bezier.js
@@ -0,0 +1,6 @@
+function bezier(){
+    var canvas = document.getElementById("app");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
